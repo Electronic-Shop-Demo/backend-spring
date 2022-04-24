0b1a9751d49f1da485df07694e4f40c954be9fb9
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-24 21:00:15 +0300

    CacheBase.java cache base abstract implementation added

---
+++ src/main/java/com/mairwunnx/application/cache/CacheBase.java
@@ -0,0 +1,46 @@
+package com.mairwunnx.application.cache;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import lombok.NonNull;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+@Log4j2
+@SuppressWarnings("unused")
+abstract class CacheBase<IN, OUT> {
+
+    private final Cache<IN, OUT> cache;
+
+    CacheBase(final long maxSize) {
+        cache = CacheBuilder.newBuilder()
+            .maximumSize(maxSize)
+            .build();
+    }
+
+    @ParametersAreNonnullByDefault
+    CacheBase(final long maxSize, final long expireAfter, final TimeUnit unit) {
+        cache = CacheBuilder.newBuilder()
+            .maximumSize(maxSize)
+            .expireAfterWrite(expireAfter, unit)
+            .build();
+    }
+
+    @ParametersAreNonnullByDefault
+    @NotNull OUT getOrCache(final IN in) {
+        try {
+            return cache.get(in, () -> getNewInstance(in));
+        } catch (final ExecutionException e) {
+            log.error("Impossible to receive cache by key {}, returned new instance!", in);
+            return getNewInstance(in);
+        }
+    }
+
+    @ParametersAreNonnullByDefault
+    abstract @NonNull OUT getNewInstance(final IN in);
+
+}
