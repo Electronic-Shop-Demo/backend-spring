2e64fd447e0a78658a4e47c044ca52c3b8b2f975
Pavel Erokhin <MairwunNx@gmail.com>
2022-04-29 20:48:15 +0300

    ImageFormatValidator.java image format validator implemented

---
+++ src/main/java/com/mairwunnx/application/valid/ImageFormatValidator.java
@@ -0,0 +1,26 @@
+package com.mairwunnx.application.valid;
+
+import com.mairwunnx.application.exception.CodeAwareException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import static com.mairwunnx.application.Constants.Errors.NO_IMAGE_EXTENSION;
+
+@Component
+public class ImageFormatValidator implements ValidatorBase<String> {
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public void validate(final String candidate) {
+        switch (candidate) {
+            case "jpg", "jpeg", "png", "webp" -> noop();
+            default -> throw new CodeAwareException(NO_IMAGE_EXTENSION, HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    private static void noop() {
+    }
+
+}
