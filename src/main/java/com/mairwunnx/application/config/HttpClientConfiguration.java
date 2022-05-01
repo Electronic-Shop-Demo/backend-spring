4a1ae0c42989f407852b0199b765ebb28e5bca38
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-01 14:56:15 +0300

    HttpClientConfiguration.java http client configuration created

---
+++ src/main/java/com/mairwunnx/application/config/HttpClientConfiguration.java
@@ -0,0 +1,22 @@
+package com.mairwunnx.application.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.http.HttpClient;
+import java.time.Duration;
+
+@Configuration
+public class HttpClientConfiguration {
+
+    @Bean
+    @NotNull
+    HttpClient httpClient() {
+        return HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(2))
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    }
+
+}
