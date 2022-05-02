d46f017fd8dec6ac08bf4211548af514b4ca3068
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-02 19:22:23 +0300

    RefreshRequestDto.java file formatted

--- src/main/java/com/mairwunnx/application/dto/request/RefreshRequestDto.java
+++ src/main/java/com/mairwunnx/application/dto/request/RefreshRequestDto.java
@@ -2,6 +2,4 @@ package com.mairwunnx.application.dto.request;
 
 import lombok.NonNull;
 
-public record RefreshRequestDto(@NonNull String email, @NonNull String refreshToken) {
-
-}
+public record RefreshRequestDto(@NonNull String email, @NonNull String refreshToken) {}
