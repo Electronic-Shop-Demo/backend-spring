8425d96deb324c7154e8e7909c7229d5106e8715
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-02 21:37:32 +0300

    ImageService.java image service contract created

---
+++ src/main/java/com/mairwunnx/application/dal/service/ImageService.java
@@ -0,0 +1,18 @@
+package com.mairwunnx.application.dal.service;
+
+import com.mairwunnx.application.entity.mongo.ImageDocument;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.UUID;
+
+public interface ImageService {
+
+    @ParametersAreNonnullByDefault
+    byte[] getImageById(final UUID id);
+
+    @ParametersAreNonnullByDefault
+    @NotNull ImageDocument insertImage(final MultipartFile file);
+
+}
