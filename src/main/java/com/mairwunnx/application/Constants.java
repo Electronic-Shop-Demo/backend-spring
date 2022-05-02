687f218bad6ee6827b9e0871fda069344d186a2f
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-02 15:29:08 +0300

    Constants.java `USER_NUMBER_ALLOWED_CHARS` constant added

--- src/main/java/com/mairwunnx/application/Constants.java
+++ src/main/java/com/mairwunnx/application/Constants.java
@@ -70,6 +70,7 @@ public final class Constants {
         public static final int USER_EMAIL_MIN_LENGTH_THRESHOLD = 5;
         public static final int USER_NUMBER_MIN_LENGTH_THRESHOLD = 10;
         public static final int USER_NUMBER_MAX_LENGTH_THRESHOLD = 15;
+        public static final String USER_NUMBER_ALLOWED_CHARS = "+()- ";
         public static final String[] DENIED_USERNAMES = {"admin", "administrator", "root"};
 
     }
