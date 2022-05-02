624127c4cc7ad98c90ec3e801dcd2ea2974858d6
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-02 19:21:24 +0300

    UserRepository.java user repository contract created

---
+++ src/main/java/com/mairwunnx/application/dal/repository/UserRepository.java
@@ -0,0 +1,33 @@
+package com.mairwunnx.application.dal.repository;
+
+import com.mairwunnx.application.entity.mongo.UserDocument;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.UUID;
+
+public interface UserRepository {
+
+    @ParametersAreNonnullByDefault
+    @Nullable UserDocument findByEmail(final String email);
+
+    @ParametersAreNonnullByDefault
+    @Nullable UserDocument findById(final String email);
+
+    @ParametersAreNonnullByDefault
+    boolean isExistByEmail(final String email);
+
+    @ParametersAreNonnullByDefault
+    @NotNull UserDocument insert(final UserDocument entity);
+
+    @ParametersAreNonnullByDefault
+    void updateRefreshTokenByEmail(final String email, final String refreshToken);
+
+    @ParametersAreNonnullByDefault
+    void removeRefreshTokenAndAddNewByEmail(final String email, final String refreshToken, final String newToken);
+
+    @ParametersAreNonnullByDefault
+    void removeCurrentRefreshTokenById(final UUID id, final String refreshToken);
+
+}
