e6b2c69a2a1ef4417fa34535fe03d5ea7e359437
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-04 20:52:38 +0300

    UserServiceImpl.java user service stub implementation added

--- src/main/java/com/mairwunnx/application/dal/service/impl/UserServiceImpl.java
+++ src/main/java/com/mairwunnx/application/dal/service/impl/UserServiceImpl.java
@@ -3,16 +3,22 @@ package com.mairwunnx.application.dal.service.impl;
 import com.mairwunnx.application.Constants;
 import com.mairwunnx.application.dal.repository.UserRepository;
 import com.mairwunnx.application.dal.service.UserService;
+import com.mairwunnx.application.dto.response.RestorePasswordResponseDto;
 import com.mairwunnx.application.dto.response.UserResponseDto;
+import com.mairwunnx.application.dto.response.UsersResponseDto;
 import com.mairwunnx.application.exception.CodeAwareException;
 import com.mairwunnx.application.mapper.SpringUserMapper;
 import com.mairwunnx.application.mapper.UserMapper;
+import com.mairwunnx.application.types.UsersSortVariant;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.ParametersAreNonnullByDefault;
 import java.util.UUID;
@@ -35,8 +41,18 @@ public final class UserServiceImpl implements UserService {
     }
 
     @Override
+    public @NotNull UsersResponseDto getUsers(final int page, @Nullable final UsersSortVariant sort) {
+        return null;
+    }
+
+    @Override
+    public @NotNull UsersResponseDto getBlockedUsers(final int page, @Nullable final UsersSortVariant sort) {
+        return null;
+    }
+
+    @Override
     @ParametersAreNonnullByDefault
-    public @NotNull UserResponseDto loadByEmail(final String email) {
+    public @NotNull UserResponseDto getUser(final String email) {
         final var user = usersRepository.findByEmail(email);
 
         if (user != null) return UserMapper.INSTANCE.entityToDto(user);
@@ -46,7 +62,7 @@ public final class UserServiceImpl implements UserService {
 
     @Override
     @ParametersAreNonnullByDefault
-    public @NotNull UserResponseDto loadById(final UUID id) {
+    public @NotNull UserResponseDto getUser(final UUID id) {
         final var user = usersRepository.findById(id);
 
         if (user != null) return UserMapper.INSTANCE.entityToDto(user);
@@ -56,7 +72,73 @@ public final class UserServiceImpl implements UserService {
 
     @Override
     @ParametersAreNonnullByDefault
-    public void removeById(final UUID id) {
+    public @NotNull UserResponseDto changeAvatar(final UUID id, final MultipartFile file) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto changeEmail(final UUID id, final String email) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto changePhone(final UUID id, final String phone) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto changePassword(final UUID id, final String oldPwd, final String newPwd) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto changeFullName(final UUID id, final String fullName) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NonNull UserResponseDto addAuthority(final UUID id, final String newAuthority) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NonNull UserResponseDto removeAuthority(final UUID id, final String removeAuthority) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto deactivateUser(final UUID id) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto activateUser(final UUID id) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NonNull RestorePasswordResponseDto requestRestorePassword(final String email) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NonNull UserResponseDto confirmRestorePassword(final String email, final UUID session, final String newPassword) {
+        return null;
+    }
+
+    @Override
+    @ParametersAreNonnullByDefault
+    public @NotNull UserResponseDto removeUser(final UUID id) {
         usersRepository.removeById(id);
     }
 
