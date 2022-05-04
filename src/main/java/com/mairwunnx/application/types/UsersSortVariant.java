e962cdc0d64d20a79284475fbc854e3b801b951d
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-04 20:47:49 +0300

    UsersSortVariant.java users sort variant enum type created

---
+++ src/main/java/com/mairwunnx/application/types/UsersSortVariant.java
@@ -0,0 +1,10 @@
+package com.mairwunnx.application.types;
+
+public enum UsersSortVariant {
+    EMAIL_ASC,
+    EMAIL_DESC,
+    FULLNAME_ASC,
+    FULLNAME_DESC,
+    CREATION_DATE_ASC,
+    CREATION_DATE_DESC
+}
