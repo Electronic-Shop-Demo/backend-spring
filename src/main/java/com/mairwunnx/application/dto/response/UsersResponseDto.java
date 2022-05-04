35dae7cc74e0310d6aac029cc4036a87f4592e12
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-05 00:00:11 +0300

    UsersResponseDto.java user response dto created

--- src/main/java/com/mairwunnx/application/dto/response/UsersResponseDto.java
+++ src/main/java/com/mairwunnx/application/dto/response/UsersResponseDto.java
@@ -1,8 +1,14 @@
 package com.mairwunnx.application.dto.response;
 
+import com.mairwunnx.application.types.UsersSortVariant;
 import lombok.NonNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
-public record UsersResponseDto(int page, int lastPage, @NonNull List<UserResponseDto> users) {
-}
+public record UsersResponseDto(
+    int page,
+    int lastPage,
+    @Nullable UsersSortVariant sort,
+    @NonNull List<UserResponseDto> users
+) {}
