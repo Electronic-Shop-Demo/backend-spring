1cff9374093bfaf390e26bd0294c6552204cc93e
Pavel Erokhin <MairwunNx@gmail.com>
2022-05-08 20:47:41 +0300

    AuthService.java auth service contract created

---
+++ src/main/java/com/mairwunnx/application/dal/service/AuthService.java
@@ -0,0 +1,25 @@
+package com.mairwunnx.application.dal.service;
+
+import com.mairwunnx.application.dto.response.LoginResponseDto;
+import com.mairwunnx.application.dto.response.RefreshResponseDto;
+import com.mairwunnx.application.dto.response.RegisterResponseDto;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.security.core.context.SecurityContext;
+
+import javax.annotation.ParametersAreNonnullByDefault;
+
+public interface AuthService {
+
+    @ParametersAreNonnullByDefault
+    @NotNull LoginResponseDto login(final String email, final String password);
+
+    @ParametersAreNonnullByDefault
+    @NotNull RegisterResponseDto register(final String username, final String password, final String email);
+
+    @ParametersAreNonnullByDefault
+    @NotNull RefreshResponseDto refresh(final String email, final String refreshToken);
+
+    @ParametersAreNonnullByDefault
+    void logout(final SecurityContext context, final String refreshToken);
+
+}
